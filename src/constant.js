aafe2657c21921129a1f2dc3e860ba997ad16756
Shailesh2510 <94972250+Shailesh2510@users.noreply.github.com>
2023-10-28 23:41:23 +0530

    Update constant.js

--- src/constant.js
+++ src/constant.js
@@ -1,6 +1,6 @@
 // # src/constant.js
     
 export const AVATAR_API = "https://ui-avatars.com/api";
-export const API = "http://localhost:1337/api";
+export const API = "https://strapi-tbux.onrender.com/api";
 export const AUTH_TOKEN = "authToken";
 export const BEARER = "Bearer";
